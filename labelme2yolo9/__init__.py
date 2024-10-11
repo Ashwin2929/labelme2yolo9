a9c551a67e1118d29323b57dab434c11994550ee
Ashwin <114656737+Ashwin2929@users.noreply.github.com>
2024-10-11 21:24:28 +0100

    Update __init__.py

--- labelme2yolo9/__init__.py
+++ labelme2yolo9/__init__.py
@@ -1,3 +1,3 @@
-# SPDX-FileCopyrightText: 2024-present Durga Prasad,D<spatiallysaying@gmail.com>
+# SPDX-FileCopyrightText: 2024-present Ashwin,P<p.ashwin79@gmail.com>
 #
 # SPDX-License-Identifier: MIT
